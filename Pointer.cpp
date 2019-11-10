b11165a880670aec053aefc099ca26fbc3db43e4
Rabia Khalid <39117847+Rabia-Khalid@users.noreply.github.com>
2019-11-09 21:19:24 -0500

    Update Pointer.cpp

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,3 +1,23 @@
+//Input Format
+
+//Input will contain two integers,  and , separated by a newline.
+
+//Output Format
+
+//You have to print the updated value of  and , on two different lines.
+
+//P.S.: Input/ouput will be automatically handled. You only have to complete the void update(int *a,int *b) function.
+
+//Sample Input
+
+// 4
+// 5
+
+//Sample Output
+// 9
+// 1
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
