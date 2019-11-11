59dfcc6d6d707dbec5cb2d53f9c05c794341b079
Rabia Khalid <39117847+Rabia-Khalid@users.noreply.github.com>
2019-11-11 02:15:36 -0500

    Create Struct.cpp

---
+++ Struct.cpp
@@ -0,0 +1,46 @@
+// struct is a way to combine multiple fields to represent a composite data structure, which further lays the foundation for
+// Object Oriented Programming. For example, we can store details related to a student in a struct consisting of his
+// age (int), first_name (string), last_name (string) and standard (int).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Sample Input
+
+15
+john
+carmack
+10
+Sample Output
+
+15 john carmack 10
